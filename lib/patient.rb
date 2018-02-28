39602a5fde4bf51a30a4e56ad9e1a96b433ee799
Jeamay Palo <jeamay.palo@gmail.com>
2018-02-28 04:25:25 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -7,4 +7,19 @@ class Patient
     @appointments = []
   end
 
+  def add_appointment(appointment)
+    @appointments << appointment
+    appointment.patient = self
+  end
+
+  def appointments
+    @appointments
+  end
+
+  def doctors
+    @appointments.collect do |appointment|
+      appointment.doctor
+    end
+  end
+
 end
